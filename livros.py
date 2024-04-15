1a0f72f23665b0c821ab56f02705b12cfc924d87
alunosNET <alunosnet@gmail.com>
2024-04-15 09:22:37 +0100

    Apagar, editar e pesquisar livros

--- livros.py
+++ livros.py
@@ -22,18 +22,33 @@ def menu_livros():
     """Menu dos livros"""
     
     while True:
-        utils.mostrar_menu("Menu Livros",["Adicionar","Listar","Editar","Apagar","Voltar"])
+        utils.mostrar_menu("Menu Livros",["Adicionar","Listar","Editar","Apagar","Pesquisar","Voltar"])
         op = utils.le_numero("Opção: ")
-        if op==5:
+        if op==6:
             break
         if op==1:
             adicionar()
         if op==2:
             listar()
         if op==3:
-            pass
+            editar()
         if op==4:
-            pass
+            apagar()
+        if op==5:
+            pesquisar()
+
+def pesquisar():
+    """Pesquisa livros por nome ou autor"""
+    utils.mostrar_menu("Escolha o campo de pesquisa",["Nome","Autor"])
+    op=utils.le_numero("Opção:")
+    if op==1:
+        campo="nome"
+    else:
+        campo="autor"
+    pesquisa=utils.le_texto(f"{campo} a pesquisar:")
+    for livro in livros:
+        if pesquisa.lower() in livro[campo].lower():
+            print(livro)
 
 def adicionar():
     """Adiciona um livro novo à lista de livros"""
@@ -80,8 +95,32 @@ def get_livro(id):
 
 def editar():
     """Edita os dados de um livro"""
-    pass
+    id=int(input("Qual o id do livro a editar?"))
+    livro=get_livro(id)
+    if livro is None:
+        print("Não existe nenhum livro com o id indicado.")
+        return
+    #Percorrer os campos do dicionário e permitir editar os dados
+    campos_nao_editar=["id","nr_emprestimos","leitor"]
+    for campo,valor in livro.items():
+        if campo in campos_nao_editar:
+            continue
+        print(valor)
+        novo=utils.le_texto(f"Novo {campo} ou enter para não alterar:")
+        if novo!="":
+            livro[campo]=novo
 
 def apagar():
     """Remove um livro da lista"""
-    pass
+    id=utils.le_numero("Qual o id do livro a apagar?")
+    livro=get_livro(id)
+    if livro is None:
+        print("Não existe nenhum livro com o id indicado.")
+        return
+    if livro['estado']!='disponível':
+        print("Só pode remover livros cujo estado é disponível")
+        return
+    op=utils.le_texto(f"Pretende remover o livro {livro['nome']}?")
+    if op in ['s','S','sim','SIM','yes','Sim','y']:
+        livros.remove(livro)
+        print(f"Livro removido com sucesso. Tem {len(livros)} livros.")
